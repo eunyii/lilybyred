a6946fcf93ab54b019c0e901bbce09ae64701fbb
bsy <cidea100@naver.com>
2024-03-15 01:34:35 +0900

    committed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import {HashRouter} from "react-router-dom"
+import {BrowserRouter,} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename={'lilybyred'}>
+  <BrowserRouter basename={'lilybyred'}>
     <App/>
-  </HashRouter>
+  </BrowserRouter>
 );
 
